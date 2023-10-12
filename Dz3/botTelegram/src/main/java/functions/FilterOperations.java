60cf16a219c511c5094e47783bf9a5bba85a8ac2
Neon <75301116+ParadoxGamesYT@users.noreply.github.com>
2023-10-12 23:51:23 +0300

    Add files via upload

---
+++ Dz3/botTelegram/src/main/java/functions/FilterOperations.java
@@ -0,0 +1,52 @@
+package functions;
+
+import commands.AppBotCommand;
+
+public class FilterOperations {
+    @AppBotCommand(name = "grayScale", description = "greyScale filter",
+            showInKeyboard = true)
+    public static float[] grayScale(float[] rgb) {
+        final float mean = (rgb[0] + rgb[1] + rgb[2]) / 3;
+        rgb[0] = mean;
+        rgb[1] = mean;
+        rgb[2] = mean;
+        return rgb;
+    }
+    @AppBotCommand(name = "onlyRed", description = "onlyRed filter",
+            showInKeyboard = true)
+    public static float[] onlyRed(float[] rgb) {
+        rgb[1] = 0;
+        rgb[2] = 0;
+        return rgb;
+    }
+    @AppBotCommand(name = "onlyGreen", description = "onlyGreen filter",
+            showInKeyboard = true)
+    public static float[] onlyGreen(float[] rgb) {
+        rgb[0] = 0;
+        rgb[2] = 0;
+        return rgb;
+    }
+    @AppBotCommand(name = "onlyBlue", description = "onlyBlue filter",
+            showInKeyboard = true)
+    public static float[] onlyBlue(float[] rgb) {
+        rgb[0] = 0;
+        rgb[1] = 0;
+        return rgb;
+    }
+
+    @AppBotCommand(name = "onlyYellow", description = "onlyYellow filter",
+            showInKeyboard = true)
+    public static float[] onlyYellow(float[] rgb) {
+        rgb[2] = 0;
+        return rgb;
+    }
+
+    @AppBotCommand(name = "onlyBlack", description = "onlyBlack filter",
+            showInKeyboard = true)
+    public static float[] onlyBlack(float[] rgb) {
+        rgb[0] = 0;
+        rgb[1] = 0;
+        rgb[2] = 0;
+        return rgb;
+    }
+}
